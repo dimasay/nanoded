0246eebf31db73315fd8a457b329e3f3bd49cf59
Dima Saiesvkii <saievskii.d@eudevgroup.com>
2019-05-04 23:44:55 +0300

    added gui

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,8 +3,17 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import processors.MessageProcessor;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Program {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("NanoDed");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(260, 100);
+        frame.setVisible(true);
+        frame.add(new JLabel("NanoDed server for Telegram was started"), BorderLayout.CENTER);
+
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
